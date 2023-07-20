30d5b848b76e857308e7a280426a60db0554f660
Your Name <you@example.com>
2023-07-20 18:42:30 +0545

    modal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,45 @@
-import React from 'react'
 
-export default function Home() {
+import React,{useState} from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import './Home.css'
+function BasicExample() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+    <>
+    <Card className='card-container' style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button className="add-item-btn" onClick={()=>handleShow()}>Add Occupy Table</Button>
+      </Card.Body>
+    </Card>
+
+
+<Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Table No</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Make Occupy
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      </>
+  );
 }
 
-
+export default BasicExample;
